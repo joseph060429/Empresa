defa5d78900de4726ed505eaae0c1e789651c3b4
jose060429 <josephsm0406@gmail.com>
2023-05-04 13:24:08 +0200

    Funcionando bien 04-05

--- Client/vue-project/src/plugins/vuetify.js
+++ Client/vue-project/src/plugins/vuetify.js
@@ -1,10 +1,18 @@
 // Styles
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+// import 'vuetify/dist/vuetify.min.css'
+// import '@fortawesome/fontawesone'
+// import "vuetify/dist/vuetify.min.css";
+
+// import "@fortawesome/fontawesome-free/css/all.css";
+
+// import { aliases, fa} from "vuetify/iconsets/fa"
 
 // Vuetify
-import { createVuetify } from 'vuetify'
+import { createVuetify } from "vuetify";
 
-export default createVuetify(
-  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
-)
+export default createVuetify();
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
