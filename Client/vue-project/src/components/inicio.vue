22a06e767ff61dfe833183703473bb255bdcfddc
jose060429 <josephsm0406@gmail.com>
2023-05-10 14:26:38 +0200

    De momento bien 10-05

--- Client/vue-project/src/components/inicio.vue
+++ Client/vue-project/src/components/inicio.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="container">
-    <h1 id="title">Acerca de nosotros</h1>
+  <v-card class="mx-auto" max-width="1000">
+    <v-img
+      class="align-end text-white"
+      height="200"
+      src="https://www.munisupepuerto.gob.pe/imagenes/escudo%20muni.png"
+      
+    >
+    </v-img>
+    <h2 id="title">Historia de Supe Puerto</h2>
+    <p id="parrafo">
+      Se tiene constancia que este territotio fue habitado hace 5,000 años por
+      la población del <strong> Áspero.</strong> De este primer asentamiento hay
+      restos arqueológicos, que se pueden visitar, donde se muestra cómo era
+      <strong> la Civilización Pesquera más Antigua de América.</strong> Entre
+      los años 900 y 1.200, destaca el dominio de la
+      <strong> Cultura Chancay </strong> en la región, que basaron su economía
+      en la agricultura, la ganadería y el comercio, creándose diferentes rutas
+      comerciales. De esta cultura quedan vestigios que muestra la enorme
+      relevancia que tuvo, los cuales se hallaron en el
+      <em> Cerro del Faro Viejo. </em>
+  <br>
+
+  
+      Con la llegada española, muchos de ellos se establecieron en Supe,
+      cambiando su nombre original <em> “Supec” </em> , por el actual, en una
+      ceremonia celebrada el 23 de enero de 1540, elevando su categoría a la de
+      <em> Villa Santa Isabel. </em>
+  
+  <br>
    
-    <p id="p">
-      El Distrito de Supe Puerto es un distrito del Perú, ubicado en la
-      Provincia de Barranca, en el Departamento de Lima.
+      En la época de las Independencias Americanas, en el siglo XIX, y más
+      concretamente en 1819, el almirante Lord Cochrane, junto a sus naves
+      <em> “La Victoria”</em> y el <em> “Lucero”</em> llegaron a la localidad
+      para satisfacerce con víveres y agua de los porteños, llegando muchos de
+      ellos a unirse al ejército del <em> General Don José de San Martín </em>,
+      alzándose las primeras voces para la Independencia del Perú, el 5 de abril
+      de ese mismo año.
     </p>
-  </div>
+
+   
+  </v-card>
 </template>
 
 <script setup></script>
@@ -16,13 +49,13 @@
   color: red;
   margin-inline: 2%;
   margin-block-end: auto;
-  font-family: "Times New Roman";
+  text-align: center;
+  
 }
-#p {
+#parrafo {
   color: aqua;
   font-family: "Times New Roman";
-  margin-left: 2%;
+  font-size:medium;
 
-  margin-right: 2%;
 }
 </style>
