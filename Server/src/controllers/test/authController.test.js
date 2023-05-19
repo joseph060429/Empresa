9702c2c26725a1c758807504df971da6bb92a238
jose060429 <josephsm0406@gmail.com>
2023-05-19 13:22:48 +0200

    testeoLogin Bien 19-05

--- Server/src/controllers/test/authController.test.js
+++ Server/src/controllers/test/authController.test.js
@@ -1,44 +1,24 @@
 const { loginUsuario, createNewUser } = require("../authController");
 const { mockResponse } = require("jest-mock-req-res");
 
-// Test registro bueno
 
-test("Registrarse", async () => {
+// Test login bueno
+test("Iniciar sesión el usuario y devolver un token", async () => {
   const req = {
     body: {
       email: "josephsm0406@gmail.com",
       password: "123456",
-      surnames: "Sepulveda",
-      name: "Joseph",
     },
-  };
+  }; 
 
   const res = mockResponse();
 
-  await createNewUser(req, res);
+  await loginUsuario(req, res);
 
   expect(res.status).toHaveBeenCalledWith(200);
   expect(res.json).toHaveBeenCalledWith({ token: expect.any(String) });
   expect(res.send).not.toHaveBeenCalled();
 });
-
-// Test login bueno
-// test("Iniciar sesión el usuario y devolver un token", async () => {
-//   const req = {
-//     body: {
-//       email: "josephsm0406@gmail.com",
-//       password: "123456",
-//     },
-//   };
-
-//   const res = mockResponse();
-
-//   await loginUsuario(req, res);
-
-//   expect(res.status).toHaveBeenCalledWith(200);
-//   expect(res.json).toHaveBeenCalledWith({ token: expect.any(String) });
-//   expect(res.send).not.toHaveBeenCalled();
-// });
 
 //Para probar que el test falla tenemos que poner un email o contraseña que no existan o coincidan
 
